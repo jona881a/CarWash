5723ad026c3f8bc57e6cf828ce6205e5183006f4
Jonas <jonaskunert@hotmail.com>
2020-10-06 14:09:51 +0200

    Arbejdet på Washes klassen og tilføjet noget funktionalitet mht earlybird rabat og tilføjet en printmetode til at vise tilgængelige vaske og priser dertil

--- src/Washes.java
+++ src/Washes.java
@@ -1,18 +1,48 @@
+import java.time.LocalTime;
+
 public class Washes {
 
     private final int ECONOMY_PRICE = 50;
     private final int STANDARD_PRICE = 80;
-    private final int DELUXE_PRICE= 120;
+    private final int DELUXE_PRICE = 120;
+    private final LocalTime EARLYBIRD_DISCOUNT_DEADLINE = LocalTime.of(14,00);
 
     public Washes() {
 
     }
 
-    public void printReciept() {
+    public int buyEconomyWash() {
+        return ECONOMY_PRICE;
+    }
 
+    public int buyStanderdWash() {
+        return STANDARD_PRICE;
+    }
+
+    public int buyDeLuxeWash() {
+        return DELUXE_PRICE;
+    }
+
+    public void printReciept() {
+    }
+
+    public double checkEarlyBirdDiscount(int washPrice) {
+        //if the time and date matches earlybird discount we include a 20% discounts
+        if(LocalTime.now() != EARLYBIRD_DISCOUNT_DEADLINE) {
+            return washPrice * 0.2;
+        }
+        return washPrice;
     }
 
     public void getStatistics() {
+
+    }
+
+    public void showWashes() {
+        System.out.println("Følgende vaske er tilgængelige: ");
+        System.out.printf("\n1. Økonomi: %dkr." + ECONOMY_PRICE);
+        System.out.printf("\n2. Standard: %dkr." + STANDARD_PRICE);
+        System.out.printf("\n3. De_Luxe: %dkr." + DELUXE_PRICE);
 
     }
 }
